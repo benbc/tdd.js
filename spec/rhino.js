e83c9f8a8b98374d7f8d9faf0023f5918c55a9bc
benbc <ben@bridesmere.com>
2010-01-09 21:06:55 +0000

    Rename generated yourlib.js to tdd.js.

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,7 +1,7 @@
 
 load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.js')
 load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.xhr.js')
-load('lib/yourlib.js')
+load('lib/tdd.js')
 load('spec/unit/spec.helper.js')
 
 JSpec
