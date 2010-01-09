156cd64c4ffe632277e25ba4fdc1699def0a938a
benbc <ben@bridesmere.com>
2010-01-09 21:14:46 +0000

    Remove references to gem paths in rhino.js.

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,10 +1,9 @@
-
-load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.js')
-load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.xhr.js')
-load('lib/tdd.js')
-load('spec/unit/spec.helper.js')
+load('spec/lib/jspec.js');
+load('spec/lib/jspec.xhr.js');
+load('lib/tdd.js');
+load('spec/unit/spec.helper.js');
 
 JSpec
-.exec('spec/unit/spec.js')
-.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
-.report()
+  .exec('spec/unit/spec.js')
+  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+  .report();
