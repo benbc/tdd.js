69e24945d9d913b188540e7317c7f2f2ae56134e
benbc <ben@bridesmere.com>
2010-01-09 21:20:25 +0000

    Remove references to gem paths in node.js.

--- spec/node.js
+++ spec/node.js
@@ -1,24 +1,23 @@
+process.mixin(require('sys'));
+require.paths.unshift('spec', 'spec/lib', 'lib');
+require('jspec');
+require('unit/spec.helper');
+require('tdd');
 
-process.mixin(require('sys'))
-require.paths.unshift('spec', '/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib', 'lib')
-require('jspec')
-require('unit/spec.helper')
-require('yourlib')
-
-quit = process.exit
-print = puts
+quit = process.exit;
+print = puts;
 
 readFile = function(path) {
-  var result
+  var result;
   require('posix')
     .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
-    .wait()
-  return result
-}
+    .addCallback(function(contents){ result = contents; })
+    .addErrback(function(){ throw new Error("failed to read file `" + path + "'"); })
+    .wait();
+  return result;
+};
 
 JSpec
   .exec('spec/unit/spec.js')
   .run({ reporter: JSpec.reporters.Terminal })
-  .report()
+  .report();
