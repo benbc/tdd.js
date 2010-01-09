ee6d09e6f9bbd45d20803c34c5b283f269f562fc
benbc <ben@bridesmere.com>
2010-01-09 20:54:33 +0000

    JSpec generated files.

---
+++ spec/rhino.js
@@ -0,0 +1,10 @@
+
+load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.js')
+load('/home/ben/.gem/ruby/1.8/gems/jspec-3.1.0/lib/jspec.xhr.js')
+load('lib/yourlib.js')
+load('spec/unit/spec.helper.js')
+
+JSpec
+.exec('spec/unit/spec.js')
+.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+.report()
