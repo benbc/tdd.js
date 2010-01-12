f493943b8c9812dd26d7ca7761c368337443633a
benbc <ben@bridesmere.com>
2010-01-12 21:59:46 +0000

    Wire everything up in init(). Raise onModelChanged.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -23,15 +23,18 @@ describe('tdd.js', function() {
     });
 
     it('populates the food options when the model changes', function() {
-      var model = { foods: ['Pancetta', 'Bayonne ham', 'Speck'] };
+      var model = {
+        foods: function() { return ['Pancetta', 'Bayonne ham', 'Speck']; }
+      };
       view(dom, model).modelChanged();
-      model.foods.each(function(f) {
+      model.foods().each(function(f) {
         dom.find('.food option').text().should.include(f);
       });
     });
 
     it('removes existing values when populating food options', function() {
-      view(dom, { foods: [] }).modelChanged();
+      var model = { foods: function () { return []; } };
+      view(dom, model).modelChanged();
       dom.find('.food option').text().should_not.include('Pate de Campagne');
     });
   });
@@ -43,18 +46,29 @@ describe('tdd.js', function() {
     });
 
     it('returns no foods by default', function() {
-      m.foods.should.eql([]);
+      m.foods().should.eql([]);
     });
 
     it('returns foods of the currently selected type', function() {
       m.select('Cheese');
-      m.foods.should.eql(['Epoisse', 'Comte']);
+      m.foods().should.eql(['Epoisse', 'Comte']);
     });
 
     it('clears the foods if an empty type is passed', function() {
       m.select('Cheese');
       m.select('');
-      m.foods.should.eql([]);
+      m.foods().should.eql([]);
+    });
+
+    it('triggers onModelChanged when the type changes', function() {
+      m.should.receive('onModelChanged');
+      m.select('Cheese');
+    });
+
+    it("doesn't trigger onModelChanged if the type didn't change", function(){
+      m.select('Cheese');
+      m.should_not.receive('onModelChanged');
+      m.select('Cheese');
     });
   });
 
