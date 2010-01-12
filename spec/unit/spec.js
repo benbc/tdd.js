c9620cf34f014d5414eb234c61666c88d925c859
benbc <ben@bridesmere.com>
2010-01-12 20:48:04 +0000

    View populates food options.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,29 +1,38 @@
 describe('tdd.js', function() {
   describe('view', function() {
-    it('should raise onTypeSelected when a type changes', function() {
-      var dom = jQuery('<form><select class="food-type"></select></form');
+    var dom;
+    before(function() {
+      dom = jQuery('<form>'
+                 + '  <select class="food-type">'
+                 + '    <option selected="selected">Meat</option>'
+                 + '  </select>'
+                 + '  <select class="food">'
+                 + '    <option>Pate de Campagne</option>'
+                 + '  </select>'
+                 + '</form>');
+    });
+
+    it('raises onTypeSelected when a type changes', function() {
       view(dom).should.receive('onTypeSelected');
       dom.find('.food-type').change();
     });
 
-    it('should pass the new selection to onTypeSelected', function() {
-      var dom = jQuery('<form><select class="food-type">'
-                     + '  <option selected="selected">Meat</option>'
-                     + '</select></form');
+    it('passes the new selection to onTypeSelected', function() {
       view(dom).should.receive('onTypeSelected').with_args('Meat');
       dom.find('.food-type').change();
     });
 
-    it('should populate the food options', function() {
-      var dom = jQuery('<form><select class="foodstuff"></select></form>');
-      var model = { foods: ['Pancetta', 'Bayonne ham',
-                            'Speck', 'Prosciutto'] };
-      var v = view(dom, model);
-      v.modelChanged();
+    it('populates the food options when the model changes', function() {
+      var model = { foods: ['Pancetta', 'Bayonne ham', 'Speck'] };
+      view(dom, model).modelChanged();
       model.foods.each(function(f) {
-        dom.should.have_child('.food option:contains('+f+')');
         dom.find('.food option').text().should.include(f);
       });
+    });
+
+    it('removes existing values when populating food options', function() {
+      view(dom, { foods: [] }).modelChanged();
+      dom.find('.food option').text().should_not.include('Pate de Campagne');
     });
   });
 });
